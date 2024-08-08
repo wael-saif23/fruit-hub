2056dabf3415e61f273b02123909e148881de0f6
wael-saif23 <ws.mywork@gmail.com>
2024-08-08 21:13:12 +0300

    feat: Integrate SVG assets and image generation
    Introduce support for SVG assets and implement automatic image asset generation using the `flutter_assets` package. This streamlines asset management and enhances UI development with scalable vector graphics.

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,20 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for photosCirclesSpashView
+  /// assets/photos/Circles-spash-view.svg
+  static const String photosCirclesSpashView = "assets/photos/Circles-spash-view.svg";
+
+  /// Assets for photosOutLogo
+  /// assets/photos/out_logo.png
+  static const String photosOutLogo = "assets/photos/out_logo.png";
+
+  /// Assets for photosPlant
+  /// assets/photos/plant.svg
+  static const String photosPlant = "assets/photos/plant.svg";
+
+  /// Assets for photosTheLogo
+  /// assets/photos/the_logo.svg
+  static const String photosTheLogo = "assets/photos/the_logo.svg";
+}
+
