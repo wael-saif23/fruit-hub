4eb02633d90ab5b3e1bb85eca9a9c32a5519fcce
wael-saif23 <ws.mywork@gmail.com>
2024-08-08 18:05:14 +0300

    feat: Implement app routing and navigation
    Sets up initial routing logic for the app, starting with the `SplashView` as the entry point. This establishes a foundation for navigating between different screens in the application.

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
+
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
-
+static const String routeName = 'splash';
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/helper_functions/on_generate_routes.dart';
 import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const FruitHub());
 }
+
 class FruitHub extends StatelessWidget {
   const FruitHub({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashView(),
+      initialRoute: SplashView.routeName,
+      onGenerateRoute: onGenerateRoute,
     );
   }
 }
