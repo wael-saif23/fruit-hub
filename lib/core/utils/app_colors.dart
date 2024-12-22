d327ebe9e575cc7f40f1d92560a6ccf2e348478d
Wael Saif <ws.mywork@gmail.com>
2024-12-22 17:05:55 +0200

    ** edit login UI
    - add light shaded gray color to app color style
    - create custom form text field
    - add text field to login screen

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -7,5 +7,6 @@ abstract class  AppColors {
   static const Color kLightGray = Color(0xFF949D9E);
   static const Color kGray = Color(0xFF4E5556);
   static const Color kblack = Color(0xFF0C0D0D);
+  static const Color kLightShadedGray = Color(0xFFE6E9EA);
   }
   
--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/core/widgets/custom_form_text_field.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -6,7 +8,31 @@ class LoginViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SingleChildScrollView(
-      child: Column(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: KHorizontalPadding),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            CustomFormTextField(
+              hintText: 'البريد الإلكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomFormTextField(
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.text,
+              suffixIcon: Icon(
+                Icons.remove_red_eye,
+                color: Color(0xffC9CECF),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
